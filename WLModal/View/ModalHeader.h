08c99948bb0e73dbf55ba8445622d99939122baa
刘光强 <liuguangqiang@qianbaocard.com>
2018-05-09 11:19:20 +0800

    添加modal的头文件

---
+++ WLModal/View/ModalHeader.h
@@ -0,0 +1,17 @@
+//
+//  ModalHeader.h
+//  WLModal
+//
+//  Created by 刘光强 on 2018/5/9.
+//  Copyright © 2018年 quangqiang. All rights reserved.
+//
+
+#ifndef ModalHeader_h
+#define ModalHeader_h
+
+#import "WLAlertModal.h"
+#import "WLInvoiceQrModal.h"
+#import "WLModal.h"
+#import "WLPurchaseInvoiceStepModal.h"
+
+#endif /* ModalHeader_h */
